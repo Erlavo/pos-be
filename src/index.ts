5df01b95581e4b623ef26e6389385cdab9284ea5
Ahmad Nurul Laiq <ahmadlaiq97@gmail.com>
2024-06-26 15:14:07 +0700

    chore: Update npm dependencies and scripts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 //import hono
-import { Hono } from "hono";
+import { Hono } from 'hono'
 
 //create a new instance of hono
-const app = new Hono();
+const app = new Hono()
 
-app.get("/", (c) => c.text("Hello Hono!"));
+app.get('/', (c) => c.text('Hello Hono!'))
 
-export default app;
+export default app
